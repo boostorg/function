79045752e0426694e6b0456a9f79f22283ce8d35
Peter Dimov <pdimov@gmail.com>
2018-11-13 18:02:22 +0200

    Revert "Added variadic support"
    This reverts commit f22fa49e078b1bbef31e2a472135e17847ab578a.

--- include/boost/function/detail/maybe_include.hpp
+++ include/boost/function/detail/maybe_include.hpp
@@ -7,18 +7,7 @@
 
 // For more information, see http://www.boost.org
 
-#include <boost/function/function_fwd.hpp> // for BOOST_FUNCTION_NO_VARIADIC
-
-#ifndef BOOST_FUNCTION_NO_VARIADIC
-#  undef BOOST_FUNCTION_NUM_ARGS
-#  define BOOST_FUNCTION_NUM_ARGS 50
-#  undef BOOST_FUNCTION_MAX_ARGS_DEFINED
-#  define BOOST_FUNCTION_MAX_ARGS_DEFINED 50
-#  ifndef BOOST_FUNCTION_VARIADIC_INCLUDED
-#    define BOOST_FUNCTION_VARIADIC_INCLUDED
-#    include <boost/function/function_template.hpp>
-#  endif
-#elif BOOST_FUNCTION_NUM_ARGS == 0
+#if BOOST_FUNCTION_NUM_ARGS == 0
 #  undef BOOST_FUNCTION_MAX_ARGS_DEFINED
 #  define BOOST_FUNCTION_MAX_ARGS_DEFINED 0
 #  ifndef BOOST_FUNCTION_0
