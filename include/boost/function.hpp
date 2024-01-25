c4237534916fa830cc597f17dd24104a38f76a06
Peter Dimov <pdimov@gmail.com>
2024-01-25 05:28:17 +0200

    Remove unnecessary includes

--- include/boost/function.hpp
+++ include/boost/function.hpp
@@ -20,17 +20,8 @@
 #define BOOST_FUNCTION_MAX_ARGS_DEFINED 0
 #endif
 
-#include <functional> // unary_function, binary_function
-
-#include <boost/preprocessor/iterate.hpp>
-#include <boost/config/workaround.hpp>
-
-// Include the prologue here so that the use of file-level iteration
-// in anything that may be included by function_template.hpp doesn't break
 #include <boost/function/detail/prologue.hpp>
-
 #include <boost/function/detail/maybe_include.hpp>
-
 #include <boost/function/detail/epilogue.hpp>
 
 #endif // !defined(BOOST_FUNCTION_MAX_ARGS_DEFINED) || (BOOST_FUNCTION_MAX_ARGS_DEFINED != BOOST_FUNCTION_MAX_ARGS)
